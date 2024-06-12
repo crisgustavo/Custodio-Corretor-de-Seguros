a2858996feb1ba4b00d401cf929f1e793c8f8612
crisgustavo <cris.gustavogs@gmail.com>
2024-06-12 10:33:09 -0300

    Ativação de função botões saiba mais e voltar

---
+++ scripts.js
@@ -0,0 +1,127 @@
+const produtosseguros = document.querySelector(".produtos-seguros")
+const produtosconsorcios = document.querySelector(".produtos-consorcio")
+
+const segauto = document.querySelector(".div-automovel")
+const segempresarial = document.querySelector(".div-empresarial")
+const segresidencial = document.querySelector(".div-residencial")
+const segvida = document.querySelector(".div-vida")
+const segportateis = document.querySelector(".div-portateis")
+const segagricola = document.querySelector(".div-agricola")
+const consauto = document.querySelector(".div-consautomovel")
+const consimovel = document.querySelector(".div-consimovel")
+
+
+
+function saibamaisauto() {
+    produtosseguros.style.visibility = "hidden"
+    produtosseguros.style.position = "fixed"
+    produtosseguros.style.left = "-1000px"
+    segauto.style.visibility = "visible"
+    segauto.style.position = "unset"
+    segauto.style.left = "auto"
+}
+
+function saibamaisempresarial() {
+    produtosseguros.style.visibility = "hidden"
+    produtosseguros.style.position = "fixed"
+    produtosseguros.style.left = "-1000px"
+    segempresarial.style.visibility = "visible"
+    segempresarial.style.position = "unset"
+    segempresarial.style.left = "auto"
+}
+
+function saibamaisresidencial() {
+    produtosseguros.style.visibility = "hidden"
+    produtosseguros.style.position = "fixed"
+    produtosseguros.style.left = "-1000px"
+    segresidencial.style.visibility = "visible"
+    segresidencial.style.position = "unset"
+    segresidencial.style.left = "auto"
+}
+
+function saibamaisvida() {
+    produtosseguros.style.visibility = "hidden"
+    produtosseguros.style.position = "fixed"
+    produtosseguros.style.left = "-1000px"
+    segvida.style.visibility = "visible"
+    segvida.style.position = "unset"
+    segvida.style.left = "auto"
+}
+
+function saibamaisportateis() {
+    produtosseguros.style.visibility = "hidden"
+    produtosseguros.style.position = "fixed"
+    produtosseguros.style.left = "-1000px"
+    segportateis.style.visibility = "visible"
+    segportateis.style.position = "unset"
+    segportateis.style.left = "auto"
+}
+
+function saibamaisagricola() {
+    produtosseguros.style.visibility = "hidden"
+    produtosseguros.style.position = "fixed"
+    produtosseguros.style.left = "-1000px"
+    segagricola.style.visibility = "visible"
+    segagricola.style.position = "unset"
+    segagricola.style.left = "auto"
+}
+
+function saibamaisautomovel() {
+    produtosconsorcios.style.visibility = "hidden"
+    produtosconsorcios.style.position = "fixed"
+    produtosconsorcios.style.left = "-1000px"
+    consauto.style.visibility = "visible"
+    consauto.style.position = "unset"
+    consauto.style.left = "auto"
+}
+
+function saibamaisimovel() {
+    produtosconsorcios.style.visibility = "hidden"
+    produtosconsorcios.style.position = "fixed"
+    produtosconsorcios.style.left = "-1000px"
+    consimovel.style.visibility = "visible"
+    consimovel.style.position = "unset"
+    consimovel.style.left = "auto"
+}
+
+function voltarprodutos() {
+    produtosseguros.style.visibility = "visible"
+    produtosseguros.style.position = "unset"
+    produtosseguros.style.left = "auto"
+
+    produtosconsorcios.style.visibility = "visible"
+    produtosconsorcios.style.position = "unset"
+    produtosconsorcios.style.left = "auto"
+
+    segauto.style.visibility = "hidden"
+    segauto.style.position = "fixed"
+    segauto.style.left = "-1000px"
+
+    segempresarial.style.visibility = "hidden"
+    segempresarial.style.position = "fixed"
+    segempresarial.style.left = "-1000px"
+
+    segresidencial.style.visibility = "hidden"
+    segresidencial.style.position = "fixed"
+    segresidencial.style.left = "-1000px"
+    
+    segvida.style.visibility = "hidden"
+    segvida.style.position = "fixed"
+    segvida.style.left = "-1000px"
+
+    segportateis.style.visibility = "hidden"
+    segportateis.style.position = "fixed"
+    segportateis.style.left = "-1000px"
+
+    segagricola.style.visibility = "hidden"
+    segagricola.style.position = "fixed"
+    segagricola.style.left = "-1000px"
+
+    consauto.style.visibility = "hidden"
+    consauto.style.position = "fixed"
+    consauto.style.left = "-1000px"
+
+    consimovel.style.visibility = "hidden"
+    consimovel.style.position = "fixed"
+    consimovel.style.left = "-1000px"
+}
